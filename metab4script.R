85ad13318a265d76747f4e0be6694d4df1b71d61
jsychong <jasmine.chong@mail.mcgill.ca>
2018-11-26 10:18:12 -0500

    Update metab4script.R

--- metab4script.R
+++ metab4script.R
@@ -1,3 +1,2 @@
-library(pacman)
-p_load(Rserve, RColorBrewer, xtable, som, fitdistrplus, ROCR, RJSONIO, gplots, e1071, caTools, igraph, randomForest, Cairo, pls, pheatmap, lattice, rmarkdown, knitr, data.table, pROC, Rcpp, caret, ellipse, scatterplot3d, impute, pcaMethods, siggenes, globaltest, GlobalAncova, Rgraphviz, KEGGgraph, preprocessCore, genefilter, SSPA, sva, limma, car, xcms)
+library(Rserve)
 Rserve(args=" --no-save --RS-conf /etc/rserve.conf")
